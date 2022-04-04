0ba6e57f24f9937d36da330f11c0adf8b661adb8
Shaikidris7786 <skidris7786@gmail.com>
2022-04-04 16:52:44 +0530

    Added theme pages, white, purple and default, You may add some more to it later

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const allSections = document.querySelectorAll('.main-content');
 
 console.log(allSections[0])
 function PageTransition() {
+
+    //Set the Theme of the Page
+    let theme = localStorage.getItem('theme')
+    if(theme == null){
+        setTheme('light')
+    }else{
+        setTheme(theme)
+    }
+
     // Button click and avtive classs
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function(){
@@ -31,6 +40,37 @@ function PageTransition() {
             element.classList.add('active');
         }
     })
+
+    // Toggle Theme Color
+    let themeBtns = document.getElementsByClassName('theme-btn');
+    
+    for (let i = 0; i < themeBtns.length;i++)
+    {
+        themeBtns[i].addEventListener('click',function(){
+            let mode = this.dataset.mode
+            setTheme(mode)
+        })
+    }
+}
+
+function setTheme(mode){
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'style/style.css'
+	}
+
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'style/white.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'style/green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'style/purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
 }
 
 PageTransition();
