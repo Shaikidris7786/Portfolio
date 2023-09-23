f8e7cc3ec6e2ad28bc426e7bd4143696b9324615
Shaik Idrisulla <51530584+Shaikidris7786@users.noreply.github.com>
2023-09-24 02:57:18 +0530

    Create events.js
    Added the script to trigger Events in Gainsight

---
+++ events.js
@@ -0,0 +1,46 @@
+var form = document.getElementById('eventsAPI')
+
+form.addEventListener('submit',function(e){
+  // Prevent auto submission of the form
+
+  e.preventDefault()
+
+  var Name = document.getElementById('Name')
+  var Email = document.getElementById('Email')
+  var Subject = document.getElementById('Subject')
+  var Message = document.getElementById('Message')
+
+  // Postman Code
+  var myHeaders = new Headers();
+  myHeaders.append("topicName", "Rule Trigger Event");
+  myHeaders.append("eventName", "Shaik HTML Test");
+  myHeaders.append("eventVersion", "1.0");
+  myHeaders.append("tenantId", "6998e9cf-b354-450f-80fe-2730b1c1dc61");
+  myHeaders.append("sharedSecret", "uy5RgBP+9FfescqwBFjU7fsE/jxH3RI/kkJ+pv5g3D5RuUAprW3+tu5f5MR95x1R");
+  myHeaders.append("accessKey", "97f48900-33a5-44f4-b591-6c7a19f01d1a");
+  myHeaders.append("Content-Type", "application/json");
+
+  console.log(myHeaders)
+
+  var raw = JSON.stringify({
+    "Subject": Subject,
+    "Message": Message,
+    "Email": Email,
+    "Name": Name
+  });
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  
+  fetch("https://demo-marscotest.gainsightcloud.com/v1.0/api/eventManager/event", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    
+  })
+  
+})
